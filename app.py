7c8cdf3d126a6d43abcc89ff9fa5568d6e864d2e
Dhruvi Mittal <dhruvimittalbsc24@rvu.edu.in>
2026-09-23 10:59:44 +0530

    Update homepage message in Flask app

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "<h1>Cloud Platforms and Service Models</h1><h2>Updated Successfully!</h2>"
+    return "<h1>Cloud Platforms Lab</h1><h2>Flask Application Successfully Deployed on Render!</h2>"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
